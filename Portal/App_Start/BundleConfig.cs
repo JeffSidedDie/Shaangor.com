a833f809739110fe767db620876af10ad3d8731a
Jeff Polakiewicz <jeff.polakiewicz@ssnetwork.com>
2015-04-09 21:45:22 -0400

    Added lato font face css file instead of using @import in bootstrap css.

--- Portal/App_Start/BundleConfig.cs
+++ Portal/App_Start/BundleConfig.cs
@@ -8,17 +8,18 @@ namespace Portal
 		public static void RegisterBundles(BundleCollection bundles)
 		{
 			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-					  "~/Scripts/jquery-{version}.js"));
+				"~/Scripts/jquery-{version}.js"));
 
 			bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-					  "~/Scripts/jquery.validate*"));
+				"~/Scripts/jquery.validate*"));
 
 			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-					"~/Scripts/bootstrap.js"));
+				"~/Scripts/bootstrap.js"));
 
 			bundles.Add(new StyleBundle("~/Content/css").Include(
-					"~/Content/bootstrap.css",
-					"~/Content/site.css"));
+				"~/Content/font-googleapis-lato-italic.css",
+				"~/Content/bootstrap.css",
+				"~/Content/site.css"));
 
 			BundleTable.EnableOptimizations=true;
 		}
